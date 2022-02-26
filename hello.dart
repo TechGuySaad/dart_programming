7ed648ec36171502ed3133933f01f17dd261cdcc
Saad Rahman <saadreh5@gmail.com>
2022-02-26 23:20:33 +0500

    different ways to use print function

---
+++ hello.dart
@@ -0,0 +1,18 @@
+void main()
+{
+  print('hello world');
+  print(" I can use both forms, single quotes or double quotes");
+  
+  print('printing a number directly without using quotation marks');
+  print(10);
+  print('\n');
+  
+  print('printing arithematic operations add, subtract, divide, multiply');
+  print( 1+2);
+  print(10/2);
+  print(2*5);
+  print(5-2);
+
+  print('can print boolean values');
+  print(false);
+}
