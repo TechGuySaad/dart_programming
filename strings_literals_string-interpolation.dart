1ffa83677395ce1f247d92bc267c0c5cb5238bb3
Saad Rahman <saadreh5@gmail.com>
2022-02-27 00:26:46 +0500

    intial commit

---
+++ strings_literals_string-interpolation.dart
@@ -0,0 +1,14 @@
+void main()
+{
+  //literals
+  //the below are the literals in dart 
+  //and other programming languages as well
+  true;
+  2;
+  "John";
+  4.5;
+
+
+  //various ways to define string literals in dart
+  //string interpolation
+}
