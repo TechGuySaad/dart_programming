e01885ae8a3cc4db9f6dc280212d9ef5db69b02a
Saad Rahman <saadreh5@gmail.com>
2022-02-26 23:28:26 +0500

    initial

--- datatypes_varts.dart
+++ datatypes_varts.dart
@@ -2,5 +2,9 @@ void main()
 {
   //third section
   //variables and data types
-  
+  //hhow to declare a variable?
+  //What are built in datatypes in dart?
+  //what are literals?
+  //sting interpolations
+  //constants in dart that is final and const keyword
 }
