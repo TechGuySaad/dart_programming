7550706369b6a73a58d658e1f234c7f08a43107c
Saad Rahman <saadreh5@gmail.com>
2022-02-26 23:26:10 +0500

    single and multiline comments

---
+++ comments.dart
@@ -0,0 +1,14 @@
+void main()
+{
+  //single line comments
+  //second line
+  /*multiline comments
+  next line*/
+
+  //just like cpp
+  //anything in comments will be ignored by the compiler
+
+  print('yayy');
+
+  //multi line comments is discouraged
+}
