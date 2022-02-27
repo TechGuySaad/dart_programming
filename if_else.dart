c3370a9e3ac66b3a64dd2ca0ff208f11b1bffe2c
Saad Rahman <saadreh5@gmail.com>
2022-02-28 00:28:24 +0500

    if,else,else if

---
+++ if_else.dart
@@ -0,0 +1,20 @@
+void main()
+{
+  print("time to do if else statements");
+
+  int x = 1;
+
+  //if else and laddered if else statments
+
+  if(x == 10)
+  {
+    print("$x is an integer");
+
+  }else if(x<10)
+  {
+    print("this is less than 10");
+  }
+  else{
+    print("something");
+  }
+}
